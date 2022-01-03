fe7c7082b9a5393faf09f49ab52b38ba7ac0d5ad
Javier <javier.bryan@speurgroup.com>
2022-01-02 23:22:41 -0500

    refactoring

--- src/services/sanity.service.js
+++ src/services/sanity.service.js
@@ -1,11 +1,11 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
-import { SANITY_PROJECT_ID, SANITY_TOKEN_ALL_ACCESS} from '../config';
+import { SANITY_DATAESET, SANITY_API_VERSION, SANITY_PROJECT_ID, SANITY_TOKEN_ALL_ACCESS} from '../config';
 
 export const client = sanityClient({
   projectId: SANITY_PROJECT_ID,
-  dataset:'production',
-  apiVersion: '2021-11-16',
+  dataset: SANITY_DATAESET,
+  apiVersion: SANITY_API_VERSION,
   useCdn:true,
   token: SANITY_TOKEN_ALL_ACCESS
 });
